e62d6e46d602eb4645b22d11aedc7c3f8b771cb2
iamsameeraliyanage <iamsameeraliyanage@gmail.com>
2023-10-11 22:49:03 +0530

    Update Dashboard Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import {
-    Button,
     CssBaseline,
-    Stack,
     StyledEngineProvider,
     ThemeProvider,
 } from "@mui/material";
 import React from "react";
 import theme from "./theme";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
     return (
@@ -14,20 +13,7 @@ function App() {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <div className="app">
-                    <Stack spacing={2} direction="row">
-                        <Button color="primary" variant="contained">
-                            Hello world
-                        </Button>
-                        <Button color="secondary" variant="contained">
-                            Hello world
-                        </Button>
-                        <Button color="success" variant="contained">
-                            Hello world
-                        </Button>
-                        <Button color="warning" variant="contained">
-                            Hello world
-                        </Button>
-                    </Stack>
+                    <Dashboard />
                 </div>
             </ThemeProvider>
         </StyledEngineProvider>
